600fd7d92416c1fe80f6200eeb7d771d3761a672
hazemosama <hoezuma@gmail.com>
2023-03-09 13:42:17 +0200

    ok

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,33 +1,33 @@
 const getAllUsers = (req, res) => {
   res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined",
+    status: 'error',
+    message: 'This route is not yet defined',
   });
 };
 const getUser = (req, res) => {
   res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined",
+    status: 'error',
+    message: 'This route is not yet defined',
   });
 };
 
 const createUser = (req, res) => {
   res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined",
+    status: 'error',
+    message: 'This route is not yet defined',
   });
 };
 
 const updateUser = (req, res) => {
   res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined",
+    status: 'error',
+    message: 'This route is not yet defined',
   });
 };
 const deleteUser = (req, res) => {
   res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined",
+    status: 'error',
+    message: 'This route is not yet defined',
   });
 };
 
